df480a222cab8843b7a2c097451b72a55366fa97
ruanJS <rm551096@fiap.com.br>
2023-09-19 12:38:20 -0300

    adicionando conteudo de experiencias

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -9,7 +9,7 @@ import "react-vertical-timeline-component/style.min.css";
 
 import { styles } from "../styles";
 import { experiences } from "../constants";
-// import { SectionWrapper } from "../hoc";
+import { SectionWrapper } from "../hoc";
 import { textVariant } from "../utils/motion";
 
 const ExperienceCard = ({ experience }) => {
@@ -61,10 +61,10 @@ const Experience = () => {
     <>
       <motion.div variants={textVariant()}>
         <p className={`${styles.sectionSubText} text-center`}>
-          What I have done so far
+          Meus feitos até agora.
         </p>
         <h2 className={`${styles.sectionHeadText} text-center`}>
-          Work Experience.
+          Experiência de trabalho.
         </h2>
       </motion.div>
 
